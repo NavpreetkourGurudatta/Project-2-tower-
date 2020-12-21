cfb0badc38ffc8d304fcfebd82e09bd74a2401f0
NavpreetkourGurudatta <74129321+NavpreetkourGurudatta@users.noreply.github.com>
2020-12-21 22:30:43 +0530

    Add files via upload

---
+++ Blocks.js
@@ -0,0 +1,25 @@
+class Blocks extends BaseClass {
+  constructor(x, y){
+    super(x,y,30,40);
+        this.Visiblity = 255;
+  }
+
+ display(){
+   console.log(this.body.speed);
+   if(this.body.speed < 3){
+    super.display();
+   }
+   else{
+     World.remove(world, this.body);
+     push();
+     this.Visiblity = this.Visiblity - 5;
+     tint(255,this.Visiblity);
+    
+     pop();
+   }
+   
+ }
+
+
+
+};
